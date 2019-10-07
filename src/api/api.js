004cfcc6c0e89cff7ddbce3482f102995a1286da
xiaoyi <630475752@qq.com>
2019-10-07 22:20:44 +0800

    修改提示

--- src/api/api.js
+++ src/api/api.js
@@ -4,8 +4,7 @@ import {setCookie} from './../js/cookieUtil'
 
 axios.defaults.baseURL = 'http://localhost:80/demo';
 axios.defaults.headers.common['Authorization'] = "";//sessionStorage.getItem('token');
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 let base = 'http://localhost:80/demo';
 
 //export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
@@ -16,7 +15,7 @@ export const requestLogin = params => {
 	axios.defaults.headers.common['Authorization'] =response.data.token; 
 	setCookie("token",response.data.token);
 	
-	return response.data;}).catch(function (error) {alert("error")});};
+	return response.data;}).catch(function (error) {alert("未知错误，请联系管理员！")});};
 //export const requestLogin = params => { return axios.post(`${base}/login`, params).then(function (response) { axios.defaults.headers.common['Authorization'] =response.data.token; return response.data}).catch(function (error) {alert("error")});};
 
 export const getUserList = params => { return axios.get(`${base}/user/listpage`, {params :params} ).then(function (response){ return response.data;}).catch(function (error) {alert("error")}); };
